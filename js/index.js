5577c147cdd59f47b62e4813b7603a2f7d375459
Luiz Otavio <otavio.tata.morais@gmail.com>
2023-05-04 21:21:15 -0300

    botao redireciona ate o topo da pagina + efeito de esconder o botao

---
+++ js/index.js
@@ -0,0 +1,25 @@
+const goTopBtn = document.querySelector('#goTopBtn');
+
+let i = 0;
+
+//hiding button
+window.addEventListener('scroll', () =>{
+    if(window.scrollY < 600){
+        goTopBtn.style.display = 'none';
+    }
+    else{
+        goTopBtn.style.display = 'block';
+    }
+})
+    
+
+
+goTopBtn.addEventListener('click', () => {
+    goTop();
+})
+
+const goTop = () => {
+    if(window.scrollY >= 600){
+        window.scrollTo({top: 0});
+    }
+}
