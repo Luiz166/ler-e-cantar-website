9b23a60c828b25a85a477864319b93298dba90fd
Luiz Otavio <otavio.tata.morais@gmail.com>
2023-05-13 21:57:14 -0300

    adicionado animacao em scrolling usando scrollRevealJS

---
+++ js/scrollReveal.js
@@ -0,0 +1,20 @@
+import 'https://unpkg.com/scrollreveal';
+
+var section2 = document.querySelector('#section2');
+var section3 = document.querySelector('#section3');
+var section4 = document.querySelector('#section4');
+
+ScrollReveal().reveal(section2, { 
+    reset: true,
+    duration: 700
+});
+
+ScrollReveal().reveal(section3, { 
+    reset: true,
+    duration: 700
+});
+
+ScrollReveal().reveal(section4, { 
+    reset: true,
+    duration: 700
+});
