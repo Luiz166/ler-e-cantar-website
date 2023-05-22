8b19ad8d6bb486be4f6e45d4c8c3b07a743a9b4e
Luiz Otavio <otavio.tata.morais@gmail.com>
2023-05-22 11:17:49 -0300

    arrumando funcionalidades do gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -4,11 +4,16 @@ const image = document.querySelectorAll('.galleryImg');
 video.forEach(play => play.addEventListener('click', () =>{
     play.classList.toggle('active');
 
-    if(play.paused){
+    if(play.classList.contains('active')){
         play.play();
-    }else{
+    }
+    else if(play.currentTime == play.duration){
         play.pause();
         play.currentTime = 0;
+    }
+    else{
+        play.pause();
+        play.currentTime = play.currentTime();
     }
 }));
 
